3fa454a2b3b7a32472d4bccf8c0cf4442f5d29ce
Nick Sanford <nicholascsanford@gmail.com>
2018-09-24 22:10:07 -0400

    (WIP) - Add web UI
    - Add websocket example as building off point including static files
    - Add WAI / Warp for hosting static files
    - TBD curently I am planning on binding the websocket server and the
    staic server on different ports, but I am not sure if it is recommended
    / possible to bind them to the same port

---
+++ src/web.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
+module WebStatic where
+
+import Network.Wai
+import Network.HTTP.Types
+import Network.Wai.Handler.Warp (run)
+
+notFound :: Response
+notFound = responseLBS status404 [("Content-Type", "text/plain")] "404 - Not Found"
+
+indexJS :: Response
+indexJS = responseFile status200 [("Content-Type", "application/javascript")] "web/index.js" Nothing
+
+index :: Response
+index = responseFile status200 [("Content-Type", "text/html")] "web/index.html" Nothing
+
+screenCSS :: Response
+screenCSS = responseFile status200 [("Content-Type", "text/css")] "web/index.css" Nothing
+
+app :: Application
+app request respond = respond $ case rawPathInfo request of
+  "/" ->
+    index
+  "/index.js" ->
+    indexJS
+  "/index.css" ->
+    screenCSS
+  _ ->
+    notFound
+
+start :: Int -> IO ()
+start port = run port app
